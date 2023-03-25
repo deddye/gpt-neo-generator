06b8abf30d39ab436cdb2d6bfc3887ac6ee811ed
Ethan Dye <42674340+deddye@users.noreply.github.com>
2023-03-25 14:18:15 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -6,9 +6,12 @@ generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
 # assign directory
 directory = sys.argv[1]
 
+# go through given directory
 for (path, dirs, fnames) in os.walk(directory):
     qualified_filenames = (os.path.join(path, filename) for filename in fnames)
     for f in qualified_filenames:
+    
+        # use only files that have "prompt" in filename
         if (f.find("prompt") > 0):
             file = open(f, "r")
             while True:
@@ -18,7 +21,10 @@ for (path, dirs, fnames) in os.walk(directory):
                     print("skipping this line bc encode error")
                 else:
                     prompt = line[5:]
+                    
+                    # generate 10 samples per prompt
                     for x in range (0,10):
+                    
                         gen_output = generator(prompt, do_sample=True, min_length=200)
                         text = gen_output[0]["generated_text"]
                         output_file = open("generated_text.txt", "a")
