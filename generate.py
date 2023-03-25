ce76b23a12c57f9e057a302d3e5aaefab54a1e06
Ethan Dye <42674340+deddye@users.noreply.github.com>
2023-03-25 14:16:47 -0400

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,31 @@
+from transformers import pipeline
+import os, sys
+
+generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
+
+# assign directory
+directory = sys.argv[1]
+
+for (path, dirs, fnames) in os.walk(directory):
+    qualified_filenames = (os.path.join(path, filename) for filename in fnames)
+    for f in qualified_filenames:
+        if (f.find("prompt") > 0):
+            file = open(f, "r")
+            while True:
+                try:
+                    line = file.readline()
+                except:
+                    print("skipping this line bc encode error")
+                else:
+                    prompt = line[5:]
+                    for x in range (0,10):
+                        gen_output = generator(prompt, do_sample=True, min_length=200)
+                        text = gen_output[0]["generated_text"]
+                        output_file = open("generated_text.txt", "a")
+                        output_file.write(text + "\n\n")
+                        output_file.close()
+
+                if not line:
+                    file.close()
+                    break
+
